673d4f036d4407a745d81cf41f551b2a3c107a0b
kitkassabar <168371784+kitkassabar@users.noreply.github.com>
2024-06-24 13:53:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,37 @@ let homeScore = 0;
 let guestScore = 0;
 
 function one() {
-    homeScore += 1
+    homeScore += 1;
     document.getElementById('home-score').innerText = homeScore;
     console.log("Home +1 button was clicked");
 }
 
 function two() {
-    homeScore += 2
+    homeScore += 2;
     document.getElementById('home-score').innerText = homeScore;
     console.log("Home +2 button was clicked");
 }
 
 function three() {
-    homeScore += 3
+    homeScore += 3;
     document.getElementById('home-score').innerText = homeScore;
     console.log("Home +3 button was clicked");
 }
 
 function guestone() {
-    guestScore += 1
+    guestScore += 1;
     document.getElementById('guest-score').innerText = guestScore;
     console.log("Guest +1 button was clicked");
 }
 
 function guesttwo() {
-    guestScore += 2
+    guestScore += 2;
     document.getElementById('guest-score').innerText = guestScore;
     console.log("Guest +2 button was clicked");
 }
 
 function guestthree() {
-    guestScore += 3
+    guestScore += 3;
     document.getElementById('guest-score').innerText = guestScore;
     console.log("Guest +3 button was clicked");
 }
-}
-
